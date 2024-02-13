09cb4f53096d6fc9db9e88b06cfc131354129b34
Noam Simcha Finkelstein <noamf.ink@proton.me>
2024-02-13 15:47:47 -0500

    add quarto python post

--- build-envs.sh
+++ build-envs.sh
@@ -2,27 +2,27 @@
 pip install jupyter
 
 # Create directory for virtualenvs
-mkdir ~/venvs
+mkdir "${HOME}/venvs"
 
 # For each post that has a requirements.txt file, create the corresponding kernel
 find "quarto-site/blog/posts" -type f -name "requirements.txt" | while read requirements_file; do
     # Get the directory of the found requirements.txt file
-    dir_path=$(dirname "$requirements_file")
+    dir_path=$(dirname "${requirements_file}")
 
     # Extract the name of the directory where requirements.txt is located
-    dir_name=$(basename "$dir_path")
+    dir_name=$(basename "${dir_path}")
 
     # Create a new directory for the venv with the name of the found directory
-    venv_path="~/venvs/${dir_name}"
+    venv_path="${HOME}/venvs/${dir_name}"
     
     # Create the virtual environment
-    python -m venv "$venv_path"
+    python -m venv "${venv_path}" --clear 
     
     # Activate the virtual environment
     source "${venv_path}/bin/activate"
     
     # Install requirements from the requirements.txt file
-    pip install -r "$requirements_file"
+    pip install -r "${requirements_file}"
 
     # register a corresponding kernel
     pip install ipykernel
